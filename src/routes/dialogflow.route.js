13cf7f17190687e5735e6595a5248523ad09ee03
fcavila1 <32888158+fcavila1@users.noreply.github.com>
2020-06-16 15:21:48 -0300

    Update dialogflow.route.js

--- src/routes/dialogflow.route.js
+++ src/routes/dialogflow.route.js
@@ -60,7 +60,7 @@ router.post('/message/text/send', async (req, res) => {
 
   console.log("Response....: " + JSON.stringify(responses));
   console.log("############################");
-  res.send(responses[1])
+//   res.send(responses)
 })
 
 router.post('/message/audio/send', upload.single('audioFile'), async (req, res) => {
